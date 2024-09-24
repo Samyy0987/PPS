854d66c2790cef6a15aee897432cdf15a68bceaa
Samruddh Shelke.... <samruddh.shelke24@sanjivani.edu.in>
2024-09-24 09:45:13 -0700

    Add files via upload

--- Main (14).cpp
+++ Main (14).cpp
@@ -1,11 +1,19 @@
-//cpp program to demonstrate the conditional operators 
-#include<iostream>
+#include <iostream>
+
 using namespace std;
-int main()
-{
-    int a=3,b=4;
-    //conditional operator 
-    int result = (a<b)? b:a;
-    cout<< "The greatest number is " << result <<endl;
-    return 0;
+
+int main() {
+int chips = 3;
+int chocolates = 2;
+int sodas = 4;
+int totalItems = chips + chocolates + sodas;
+int costPerChip = 20;
+int costPerChocolate = 50;
+int costPerSoda = 30;
+int totalCost = (chips * costPerChip) + (chocolates *
+costPerChocolate) + (sodas * costPerSoda);
+// Output the results
+cout << "Total Number of Items: " << totalItems << endl;
+cout << "Total Cost: " << totalCost << " Rupees" << endl;
+return 0;
 }
